91bb963d249ac33520a5cc8538d131686dddc828
Ruslan Kuga <rkdumle@gmail.com>
2021-03-27 15:14:37 +0300

    1st commit

---
+++ Makefile
@@ -0,0 +1,13 @@
+#!make
+include .env
+export
+
+
+start:
+	docker-compose up -d
+
+stop:
+	docker-compose stop
+
+db:
+	docker exec -it db mariadb -u root -p$(DB_PASSWORD) $(DB_DATABASE)
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/news', function () {
+    return view('news.index');
+});
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+
