93c88a1b4fb9f7e1b91b60f735286ec9fa9c0478
Ruslan Kuga <rkkuga@gmail.com>
2021-07-05 16:13:02 +0300

    merge issue

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/news', [NewsController::class, 'index']);
+Route::get('/news', function () {
+    return view('news.index');
+});
 
 Route::get('/', function () {
     return view('welcome');
